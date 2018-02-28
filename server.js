c6a6037f7d20e827f89fa7400ab914955ef9df99
Daan van Santen <daansan@gmail.com>
2018-02-28 17:17:45 +0100

    ingenico debugging

--- server.js
+++ server.js
@@ -79,6 +79,7 @@ app.get('/webhook', (req, res) => {
 app.get('/orderdone', handlers['orderdone']);
 
 app.post('/webhook', async(req, res) => {
+  console.log('Webhook routine hit');
   let events = req.body.entry[0].messaging;
   for (let i = 0; i < events.length; i++) {
     let event = events[i];
