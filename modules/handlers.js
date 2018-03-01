d896a90cb100ad4e5d843600ebe81c9e67a3124f
Daan van Santen <daansan@gmail.com>
2018-03-01 13:55:26 +0100

    ingenico debugging

--- modules/handlers.js
+++ modules/handlers.js
@@ -2,15 +2,12 @@
 
 let messenger = require('./messenger'),
 formatter = require('./formatter'),
-hueLights = require('./hue-lights'),
 uploads = require('./uploads');
 
 
 exports.orderdone = (req,res) => {
   res.sendStatus(200);
   console.log('Payment Ingenico Done');
-  hueLights.blink(req.query.shipType);
-  setTimeout(function(){hueLights.off(req.query.shipType);},10000);
   messenger.send(formatter.recu(req.query.shipType), req.query.sender);
 };
 
@@ -26,14 +23,6 @@ exports.Greetings = async(sender) => {
       messenger.send(formatter.information(response), sender);
     }, 2000)
   }
-};
-
-exports.InfoShip = (sender,text) => {
-  let str=text.replace('-','').replace(' ','').toLowerCase();
-  if(str.includes("xwing")){uploads.doAct(sender,"X-Wing");}
-  else if (str.includes("tiefighter")){uploads.doAct(sender,"Tie Fighter");}
-  else if (str.includes("uwing")){uploads.doAct(sender,"U-Wing");}
-  else {messenger.send(formatter.shipChoice(), sender);}
 };
 
 exports.InfoCar = (sender,text) => {
